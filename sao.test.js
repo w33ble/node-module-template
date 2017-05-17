499129590359337199213ee5a054f452fb59e198
Joe Fleming <joe.fleming@gmail.com>
2017-05-17 11:37:11 -0700

    add copyright test

---
+++ sao.test.js
@@ -0,0 +1,22 @@
+const { resolve } = require('path');
+const test = require('ava');
+const sao = require('sao');
+
+const template = {
+  fromPath: resolve('.'),
+};
+
+test('Copyright has correct info', (t) => {
+  return sao.mockPrompt(template, {
+    username: 'someuser',
+    name: 'Some User',
+  })
+  .then((res) => {
+    const thisYear = new Date().getFullYear();
+    const content = res.files['LICENSE'].contents.toString();
+    const headerText = content.match(/Copyright(.*)/)[1];
+    t.not(headerText.indexOf(thisYear), -1);
+    t.not(headerText.indexOf('github.com/someuser'), -1);
+    t.not(headerText.indexOf('Some User'), -1);
+  });
+});
