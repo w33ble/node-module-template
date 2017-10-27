74a15a6f19c89686e364902fb7e3687355c2534e
Joe Fleming <joe.fleming@gmail.com>
2017-10-27 15:52:36 -0700

    chore: update the tests for build target

--- sao.test.js
+++ sao.test.js
@@ -63,14 +63,34 @@ test('ava is included when selected', (t) => {
   });
 });
 
-test('includes babel build when selected', (t) => {
+test('no babel build when none is selected', (t) => {
   return sao.mockPrompt(template, {
-    babelcjs: true,
+    buildTarget: 'none',
   })
   .then((res) => {
     const pkg = getPkg(res);
+    // no babel config
+    t.is(pkg.babel, undefined);
+    // no build script
+    t.is(pkg.scripts['build'], undefined);
+  });
+});
+
+test('includes babel build when selected', (t) => {
+  const findEnvPreset = (presets) => presets.find(preset => preset === 'env' || preset[0] === 'env');
+
+  return sao.mockPrompt(template, {
+    buildTarget: '6',
+  })
+  .then((res) => {
+    const pkg = getPkg(res);
+    // includes babel config
+    t.not(pkg.babel, undefined);
+    const config = findEnvPreset(pkg.babel.presets)[1];
+    t.is(config.targets.node, '6');
+    // includes build scripts
     t.not(pkg.scripts['build'], undefined);
     t.is(pkg.scripts['test'].match('npm run build').index, 0);
     t.is(pkg.scripts['test:only'].match('npm run build').index, 0);
-  })
-})
+  });
+});
